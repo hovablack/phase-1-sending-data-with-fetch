34aed7acbec5be3f6a00a2eeeaec1b4cc286bcb8
Hova Black <nevermind92@hotmail.com>
2022-03-04 09:47:17 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+function submitData(name, email) {
+  const destinationURL = "http://localhost:3000/users"
+
+  return fetch(destinationURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+  .then(res => res.json())
+  .then(userData => document.body.innerHTML = userData["id"])
+  .catch(function (error) {
+    document.body.innerHTML = error.message
+  })
+}
